1e38a5527fd84d491f315b3575afc3ec140dbe88
Darshak Barad <darshak.barad1212@gmail.com>
2026-06-03 22:52:07 +0530

    refactor timer functionality; add start and pause button event handlers in HTML

--- app.js
+++ app.js
@@ -2,22 +2,26 @@ const docHour = document.querySelector(".hour");
 const docMin = document.querySelector(".min");
 const docSec = document.querySelector(".sec");
 
-let time = Date.now();
+let startInterval;
 
-// const startTimer = () => {
+const startTimer = () => {
+  let time = Date.now();
+  startInterval = setInterval(() => {
+    // console.log(time);
+    let elapsed = Date.now() - time;
 
-// };
-setInterval(() => {
-  let elapsed = Date.now() - time;
+    let ms = Math.floor((elapsed % 1000) / 10);
+    let sec = (elapsed % (1000 * 60)) / 1000;
+    let min = (elapsed % (1000 * 60 * 60)) / 60000;
+    let hour = (elapsed / (1000 * 60 * 60 * 60)) % 12;
 
-  let ms = Math.floor((elapsed % 1000) / 10);
-  let sec = (elapsed % (1000 * 60)) / 1000;
-  let min = (elapsed % (1000 * 60 * 60)) / 60000;
-  let hour = (elapsed / (1000 * 60 * 60 * 60)) % 12;
+    docHour.innerText = Math.floor(hour).toString().padStart(2, "0");
+    docMin.innerText = Math.floor(min).toString().padStart(2, "0");
+    docSec.innerText = Math.floor(sec).toString().padStart(2, "0");
+    // console.log(Math.floor(hour), " : ", Math.floor(min), " : ", Math.floor(sec));
+  }, 10);
+};
 
-  docHour.innerText = Math.floor(hour).toString().padStart(2, "0");
-  docMin.innerText = Math.floor(min).toString().padStart(2, "0");
-  docSec.innerText = Math.floor(sec).toString().padStart(2, "0");
-  // console.log(Math.floor(hour), " : ", Math.floor(min), " : ", Math.floor(sec));
-}, 10);
-// startTimer();
+const pauseTimer = () => {
+  clearInterval(startInterval);
+};
