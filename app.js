f9d76e56b3a09d7596d1cf803ed6f9331abce512
Darshak Barad <darshak.barad1212@gmail.com>
2026-06-04 23:14:01 +0530

    refactor timer functionality; update pause button to remove inline event handler

--- app.js
+++ app.js
@@ -2,26 +2,46 @@ const docHour = document.querySelector(".hour");
 const docMin = document.querySelector(".min");
 const docSec = document.querySelector(".sec");
 
-let startInterval;
+let timeInterval, startTime, isRunning;
+let elapsedTime = 0;
 
 const startTimer = () => {
-  let time = Date.now();
-  startInterval = setInterval(() => {
-    // console.log(time);
-    let elapsed = Date.now() - time;
+  isPaused = false;
+  if (!isRunning) {
+    isRunning = true;
+    startTime = Date.now();
 
-    let ms = Math.floor((elapsed % 1000) / 10);
-    let sec = (elapsed % (1000 * 60)) / 1000;
-    let min = (elapsed % (1000 * 60 * 60)) / 60000;
-    let hour = (elapsed / (1000 * 60 * 60 * 60)) % 12;
+    timeInterval = setInterval(() => {
+      let elapsed = elapsedTime + (Date.now() - startTime);
 
-    docHour.innerText = Math.floor(hour).toString().padStart(2, "0");
-    docMin.innerText = Math.floor(min).toString().padStart(2, "0");
-    docSec.innerText = Math.floor(sec).toString().padStart(2, "0");
-    // console.log(Math.floor(hour), " : ", Math.floor(min), " : ", Math.floor(sec));
-  }, 10);
+      let ms = Math.floor((elapsed % 1000) / 10);
+      let sec = (elapsed % (1000 * 60)) / 1000;
+      let min = (elapsed % (1000 * 60 * 60)) / 60000;
+      let hour = (elapsed / (1000 * 60 * 60 * 60)) % 12;
+
+      docHour.innerText = Math.floor(hour).toString().padStart(2, "0");
+      docMin.innerText = Math.floor(min).toString().padStart(2, "0");
+      docSec.innerText = Math.floor(sec).toString().padStart(2, "0");
+    }, 10);
+  } else {
+    return;
+  }
 };
 
 const pauseTimer = () => {
-  clearInterval(startInterval);
+  clearInterval(timeInterval);
 };
+
+const pause = document.querySelector(".pause");
+let isPaused;
+
+pause.addEventListener("click", () => {
+  if (!isPaused) {
+    isPaused = true;
+    let pausedTime = pauseTimer();
+    elapsedTime += Date.now() - startTime;
+    isRunning = false;
+  } else {
+    isPaused = true;
+  }
+});
