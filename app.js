5544d21ba6fa0d223c29798f5608204c9f78d271
Darshak Barad <darshak.barad1212@gmail.com>
2026-06-07 22:26:59 +0530

    refactor timer functionality; update pause button to use pauseTimer function

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const docHour = document.querySelector(".hour");
 const docMin = document.querySelector(".min");
 const docSec = document.querySelector(".sec");
 
-let timeInterval, startTime, isRunning;
+let timeInterval,
+  startTime,
+  isRunning = false;
 let elapsedTime = 0;
 
 function showNotification(title, body) {
@@ -20,20 +22,21 @@ let isNotified = false;
 
 const startTimer = () => {
   isPaused = false;
+  console.log("isRunning?", isRunning);
   if (!isRunning) {
     isRunning = true;
     startTime = Date.now();
 
     timeInterval = setInterval(() => {
       let elapsed = elapsedTime + (Date.now() - startTime);
+      console.log("ElapsedTime = ", elapsedTime, "\nstartTime = ", startTime);
 
       let ms = Math.floor((elapsed % 1000) / 10);
       let sec = (elapsed % (1000 * 60)) / 1000;
       let min = (elapsed % (1000 * 60 * 60)) / 60000;
       let hour = (elapsed / (1000 * 60 * 60 * 60)) % 12;
 
-      if (Math.floor(min) === 2 && !isNotified) {
-        console.log("Notification sent!");
+      if (Math.floor(min) === 5 && !isNotified) {
         showNotification("JSTimer Notification!", "10 Seconds has completed!");
         isNotified = true;
       }
@@ -47,26 +50,30 @@ const startTimer = () => {
   }
 };
 
+let isPaused = true;
+let isReset = false;
+
 const pauseTimer = () => {
   clearInterval(timeInterval);
-};
-
-const pause = document.querySelector(".pause");
-let isPaused;
-
-pause.addEventListener("click", () => {
-  if (!isPaused) {
+  if (!isPaused && !isReset) {
     isPaused = true;
-    let pausedTime = pauseTimer();
     elapsedTime += Date.now() - startTime;
     isRunning = false;
-  } else {
+  } else if (!isPaused && isReset) {
     isPaused = true;
+    elapsedTime = 0;
+    isRunning = false;
+  } else {
+    isPaused = false;
   }
-});
+};
 
 const reset = () => {
   clearInterval(timeInterval);
+  isReset = true;
+  // startTime = 0;
+  elapsedTime = 0;
+  isRunning = false;
   docHour.innerText = "00";
   docMin.innerText = "00";
   docSec.innerText = "00";
